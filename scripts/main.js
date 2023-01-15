ad7a01ca47f6e1111c941ac75f7cd8332259bfd1
Hitesh Bhagwat <111442899+hiteshdevs@users.noreply.github.com>
2023-01-16 00:33:35 +0530

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+// header
+let toggleBtn = document.querySelector("#navbar-toggle");
+let collapse = document.querySelector("#navbar-collapse");
+
+toggleBtn.onclick = () => {
+  collapse.classList.toggle("hidden");
+  collapse.classList.toggle("flex");
+};
